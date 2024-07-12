7ef60d535277d58bb932cf2930c027f69addf9b5
lalabears <132989641+lalabears@users.noreply.github.com>
2024-07-12 10:30:11 +0900

    폼

---
+++ a0712/src/main/java/com/java/www/controller/FController.java
@@ -0,0 +1,22 @@
+package com.java.www.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class FController extends HttpServlet {
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("FController");
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+}
